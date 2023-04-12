0a52af25324ef115d507a17841ede3bbd5e2aeaf
Lucas <lucasdgonzalez@outlook.com>
2023-04-11 21:05:03 -0300

    Crea la estructura del modo intermedio
    La funcionalidad del modo intermedio está en este archivo, y utiliza muchas funciones globales del archivo index.js

---
+++ modo-intermedio.js
@@ -0,0 +1,63 @@
+const $cuadrosGrillaIntermedio = document.querySelectorAll(".cuadro-intermedio");
+
+let coloresDificultadIntermedio = [
+  "color-rojo",
+  "color-azul",
+  "color-verde",
+  "color-amarillo",
+  "color-naranja",
+  "color-rosa",
+  "color-violeta",
+  "color-marron",
+];
+
+function gestionarModoIntermedio() {
+  if (posicionesColores.length === 0) {
+    ocultarBotonJugar();
+    mostrarBotonReset();
+    asignarOrdenColores($cuadrosGrillaIntermedio);
+    pintarCuadros($cuadrosGrillaIntermedio);
+  } else {
+    return false;
+  }
+}
+
+$cuadrosGrillaIntermedio.forEach(function (cuadro) {
+  cuadro.addEventListener("click", (e) => {
+    if (juegoComenzado && cuadrosElegidos.length < 2) {
+      if (comprobarColoresEncontrados(e) === "") {
+        if (indicadorTurnos === 1) {
+          indicadorTurnos++;
+          guardarCuadroElegido(e);
+          mostrarCuadroElegido(e);
+        } else if (indicadorTurnos === 2) {
+          if (comprobarCuadroElegido(e) === "") {
+            guardarCuadroElegido(e);
+            mostrarCuadroElegido(e);
+
+            if (comprobarColoresCuadros() === "") {
+              guardarColoresEncontrados();
+              comprobarEstadoJuego();
+              actualizarTituloJuegoTerminado();
+              cuadrosElegidos = [];
+              indicadorTurnos = 1;
+            } else {
+              indicadorTurnos = 1;
+
+              setTimeout(function () {
+                ocultarCuadrosElegidos();
+                cuadrosElegidos = [];
+              }, 0850);
+            }
+          } else {
+            return false;
+          }
+        }
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  });
+});
