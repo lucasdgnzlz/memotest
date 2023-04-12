8a034efca619ef027289bc39490f5fd2443119d5
Lucas <lucasdgonzalez@outlook.com>
2023-04-11 21:03:51 -0300

    Crea la estructura del modo facil
    Toda la funcionalidad del modo facil está en este archivo .js, que utiliza varias funciones generales de index.js

---
+++ modo-facil.js
@@ -0,0 +1,61 @@
+const $cuadrosGrillaFacil = document.querySelectorAll(".cuadro-facil");
+
+const coloresDificultadFacil = [
+  "color-rojo",
+  "color-azul",
+  "color-verde",
+  "color-amarillo",
+  "color-naranja",
+  "color-rosa",
+];
+
+function gestionarModoFacil() {
+  if (posicionesColores.length === 0) {
+    ocultarBotonJugar();
+    mostrarBotonReset();
+    asignarOrdenColores($cuadrosGrillaFacil);
+    pintarCuadros($cuadrosGrillaFacil);
+  } else {
+    return false;
+  }
+}
+
+$cuadrosGrillaFacil.forEach(function (cuadro) {
+  cuadro.addEventListener("click", (e) => {
+    if (juegoComenzado && cuadrosElegidos.length < 2) {
+      if (comprobarColoresEncontrados(e) === "") {
+        if (indicadorTurnos === 1) {
+          indicadorTurnos++;
+          guardarCuadroElegido(e);
+          mostrarCuadroElegido(e);
+        } else if (indicadorTurnos === 2) {
+          if (comprobarCuadroElegido(e) === "") {
+            guardarCuadroElegido(e);
+            mostrarCuadroElegido(e);
+
+            if (comprobarColoresCuadros() === "") {
+              guardarColoresEncontrados();
+              comprobarEstadoJuego();
+              actualizarTituloJuegoTerminado();
+              cuadrosElegidos = [];
+              indicadorTurnos = 1;
+            } else {
+              indicadorTurnos = 1;
+
+              setTimeout(function () {
+                ocultarCuadrosElegidos();
+                cuadrosElegidos = [];
+              }, 0850);
+            }
+          } else {
+            return false;
+          }
+        }
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  });
+});
